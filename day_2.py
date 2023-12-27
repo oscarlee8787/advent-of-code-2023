73f4f1e0c258ad9ab75c6f68f1cbdb30b88a1290
Oscar Lee <oscarlee8787@gmail.com>
2023-12-27 17:20:53 +0100

    wip

--- day_2.py
+++ day_2.py
@@ -20,4 +20,18 @@ Determine which games would have been possible if the bag had been loaded with o
 What is the sum of the IDs of those games?
 '''
 
+example = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+'''
+
 def game(text):
+    '''turn input into list of games
+    turn list into list of dictionaries
+    '''
+    text_list = text.splitlines()
+    print(text_list)
+    return
+game(example)
